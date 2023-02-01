0bda28f52ff33d8585e219ab5b009c2f11c20f79
Hamid Berkaine <hamiddev78@gmail.com>
2023-02-01 21:51:30 +0100

    create route to blog page

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient()
 
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
     res.send('The sedulous hyena ate the antelope!');
 });
+
+app.post('/blog', async (req, res) => {
+    const blog = await prisma.blog.create({ data: req.body})
+    res.json(blog);
+})
 
 app.listen(port, () => {
     return console.log(`server is listening on ${port}`);
