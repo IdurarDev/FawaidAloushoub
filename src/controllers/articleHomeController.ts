db9aa20fc3f7cd49f8894f130ebcc6cad58e96c4
Hamid Berkaine <hamiddev78@gmail.com>
2023-02-03 22:34:40 +0100

    add article page controller

---
+++ src/controllers/articleHomeController.ts
@@ -0,0 +1,34 @@
+import { Request, Response, Router } from 'express';
+import { Logger } from 'tslog';
+
+export default class ArticleHomeController {
+    public router: Router;
+    private logger: Logger;
+
+    constructor(logger: Logger) {
+        this.router = Router();
+        this.logger = logger;
+        this.routesApp();
+    }
+
+    getAll = async (req: Request, res: Response) => {
+        this.logger.info('#getAll() called in article home page')
+        return res.send('Marhaban bikoum to the article home page')
+    }
+
+    createOne = async () => { }
+
+    getOne = async () => { }
+
+    updateOne = async () => { }
+
+    deleteOne = async () => { }
+
+    routesApp = () => {
+        this.router.get('/article', this.getAll)
+        this.router.post('/article', this.createOne)
+        this.router.get('/:id', this.getOne)
+        this.router.put('/:id', this.updateOne)
+        this.router.delete('/:id', this.deleteOne)
+    }
+}
