b429412ef5c7d288ee41a4c61ee1773e2cc8b140
Hamid Berkaine <hamiddev78@gmail.com>
2023-02-03 11:41:08 +0100

    refacto and modif urls

--- src/configs/urlConfig.ts
+++ src/configs/urlConfig.ts
@@ -5,9 +5,9 @@ env.config()
 const PROD = true
 
 export const urlConfig = {
+    homePageUrl: `${process.env.URL_ENTRY}/`,
     blogUrl: `${process.env.URL_ENTRY}/blog`,
-    articleUrl: `${process.env.URL_ENTRY}/article`,
-    planteUrl: `${process.env.URL_ENTRY}/plante`,
-    homePageUrl: `${process.env.URL_ENTRY}/homePage`,
+    articleUrl: `${process.env.URL_ENTRY}/articles`,
+    planteUrl: `${process.env.URL_ENTRY}/plantes`,
     redirectUrl: PROD ? 'https://hamid.berkaine.com' : 'http://localhost:3000'
 }
