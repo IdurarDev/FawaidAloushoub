cda087f3048d6143d0699f71871f2013e22617cc
Hamid Berkaine <hamiddev78@gmail.com>
2023-02-07 22:27:45 +0100

    update confiUrl file

--- src/configs/urlConfig.ts
+++ src/configs/urlConfig.ts
@@ -5,10 +5,10 @@ env.config()
 const PROD = true
 
 export const urlConfig = {
-    homePageUrl: `${process.env.URL_ENTRY}/`,
-    blogUrl: `${process.env.URL_ENTRY}/blog`,
-    articleUrl: `${process.env.URL_ENTRY}/articles`,
-    planteUrl: `${process.env.URL_ENTRY}/plantes`,
-    profileUrl: `${process.env.URL_ENTRY}/profile`,
+    homePageUrl: `${process.env}/`,
+    blogUrl: `${process.env}/blog`,
+    articleUrl: `${process.env}/articles`,
+    planteUrl: `${process.env}/plantes`,
+    profileUrl: `${process.env}/profile`,
     redirectUrl: PROD ? 'https://hamid.berkaine.com' : 'http://localhost:3000'
 }
